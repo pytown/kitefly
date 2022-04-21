8fa6403cf324c35d1828c3c5698fdbea16396c9f
Chris Carpita <ccarpita@gmail.com>
2022-04-21 00:19:23 -0400

    build: Fix bugs, improve test coverage, add Github Actions for CI

---
+++ tests/test_git_filter.py
@@ -0,0 +1,45 @@
+import os
+
+from kitefly import GitFilter, Command, Target, Pipeline
+import kitefly.filter.git_filter
+
+
+class GitMocked:
+    def __init__(self, file_list: list[str]):
+        self.file_list = file_list
+        self._orig_check_call = None
+        self._orig_check_output = None
+
+    def __enter__(self):
+        self.orig_check_call = kitefly.filter.git_filter.check_call
+        self.orig_check_output = kitefly.filter.git_filter.check_output
+        kitefly.filter.git_filter.check_call = self._check_call
+        kitefly.filter.git_filter.check_output = self._check_output
+
+    def __exit__(self, _1, _2, _3):
+        kitefly.filter.git_filter.check_call = self._orig_check_call
+        kitefly.filter.git_filter.check_output = self._orig_check_output
+
+    def _check_call(self, *args, **kwargs):
+        return True
+
+    def _check_output(self, cmd, *args, **kwargs):
+        if len(cmd) < 2:
+            return ""
+        if cmd[1] == "rev-parse":
+            return os.getcwd()
+        elif cmd[1] == "diff":
+            return "\n".join(self.file_list) + "\n"
+
+
+def test_git_filter():
+    with GitMocked(["README.md", "app/file.py"]):
+        c1 = Command("Run cmd", "cmd.sh", targets=[Target("**/*.py")])
+        c2 = Command("Library Tests", "libtest.sh", targets=[Target("lib/**/*.py")])
+        steps = Pipeline(c1, c2).filtered(GitFilter(base_branch="main")).steps()
+        assert len(steps) == 1
+        assert steps[0].key == "run_cmd"
+
+        # Filtering should not happen without a base branch
+        steps = Pipeline(c1, c2).filtered(GitFilter()).steps()
+        assert len(steps) == 2
---
+++ tests/test_step.py
@@ -0,0 +1,84 @@
+from kitefly import Step, Command, Target
+
+
+def test_tags():
+    class HighMem(Command):
+        tags = ["highmem"]
+
+    class HighCpu(HighMem):
+        tags = ["highcpu"]
+
+    c = HighCpu("some step", "step.sh", tags=["step"])
+    assert c.get_tags() == ["highcpu", "highmem", "step"]
+
+
+def test_targets():
+    class PyTest(Command):
+        targets = [Target("**/*.py")]
+        agents = {"pyenv": "software"}
+
+    c = PyTest("Run Python Tests", "pytest .", targets=[Target("**/test-data.json")])
+    patterns = []
+    for t in c.get_targets():
+        for p in t.patterns:
+            patterns.append(str(p))
+    assert patterns == ["r/.*[^/]*\\.py/", "r/.*test\\-data\\.json/"]
+
+
+def test_attrs():
+    c = Command(
+        "Run command",
+        "command.sh",
+        priority=10,
+        when="pr.branch == 'develop'",
+        allow_dependency_failure=True,
+        branches="my-branches",
+        other="foobar",
+    )
+    assert c.asdict() == {
+        "allow_dependency_failure": True,
+        "branches": "my-branches",
+        "command": "command.sh",
+        "if": "pr.branch == 'develop'",
+        "key": "run_command",
+        "label": "Run command",
+        "other": "foobar",
+        "priority": 10,
+    }
+
+
+def test_equality():
+    c1 = Command("Run command", "command.sh", priority=5)
+    c2 = Command("Run command", "command.sh", priority=10)
+    c3 = Command("Run command", "command.sh", priority=10)
+    c3.key = c2.key
+    assert c1 != c2
+    assert c2 == c2
+    assert c3 == c2
+    assert c1 != "foobar"
+
+
+def test_invalid_deps():
+    c1 = Command("Run command", "command.sh", priority=5)
+    c2 = Command(label="", command="command.sh")
+    c3 = Command("Valid dep", "dep.sh")
+
+    assert c2.key is ""
+
+    value_errors = 0
+    try:
+        c2 << c1
+    except ValueError:
+        value_errors += 1
+
+    assert value_errors == 1
+
+    try:
+        c1 >> c2
+    except ValueError:
+        value_errors += 1
+
+    assert value_errors == 2
+
+    c1 >> c3
+    assert c1.depends_on == ["valid_dep"]
